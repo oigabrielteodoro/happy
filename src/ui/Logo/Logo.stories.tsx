fde679aa997ffbe9186787f11ac66e1e98d79b4b
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-05-08 23:57:14 -0300

    docs: add logo in storybook

---
+++ src/ui/Logo/Logo.stories.tsx
@@ -0,0 +1,11 @@
+import type { ComponentMeta } from '@storybook/react'
+
+import { Logo } from '.'
+
+export default {
+  title: 'Logo',
+  component: Logo,
+  decorators: [(Story) => <div>{Story()}</div>],
+} as ComponentMeta<typeof Logo>
+
+export const Default = () => <Logo />
